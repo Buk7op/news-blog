4751475ef986aa83920500762f674ada468f5b09
Buk7op <victor_82hp@mail.ru>
2022-09-02 18:09:24 +0300

    added register request interface

--- src/app/auth/store/actions/register.action.ts
+++ src/app/auth/store/actions/register.action.ts
@@ -1,7 +1,8 @@
 import { createAction, props } from '@ngrx/store';
+import { RegisterRequestInteface } from '../../types/registerRequest.inteface';
 import { ActionTypes } from '../actionTypes';
 
 export const registerAction = createAction(
   ActionTypes.REGISTER,
-  props<{ username: string; email: string; password: string }>()
+  props<RegisterRequestInteface>()
 );
---
+++ src/app/auth/types/registerRequest.inteface.ts
@@ -0,0 +1,7 @@
+export interface RegisterRequestInteface {
+  user: {
+    email: string;
+    username: string;
+    password: string;
+  };
+}
