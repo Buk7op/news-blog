51620e73469779d645d62c873a8dfe937305588a
Buk7op <victor_82hp@mail.ru>
2022-09-02 18:09:11 +0300

    added ngRx, store and registery action

--- src/app/auth/components/register/register.component.ts
+++ src/app/auth/components/register/register.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { Store } from '@ngrx/store';
+import { registerAction } from '../../store/actions/register.action';
 
 @Component({
   selector: 'nb-register',
@@ -8,7 +10,7 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 })
 export class RegisterComponent implements OnInit {
   form!: FormGroup;
-  constructor(private fb: FormBuilder) {}
+  constructor(private fb: FormBuilder, private store: Store) {}
 
   ngOnInit(): void {
     this.initializeForm();
@@ -24,5 +26,6 @@ export class RegisterComponent implements OnInit {
 
   onSubmit() {
     console.log(this.form.value);
+    this.store.dispatch(registerAction(this.form.value));
   }
 }
